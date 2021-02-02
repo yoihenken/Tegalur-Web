46b4266e103cc72f801d06d99d6e9379c4168f15
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-02-02 23:22:57 +0700

    add call api detail

---
+++ app/Http/Controllers/DetailController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class DetailController extends Controller
+{
+
+    public function getNews($page, $index){
+        $list = $this->callApiDetail("NEWS", $page, $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+
+    public function getEvent($page, $index){
+        $list = $this->callApiDetail("EVENT", $page, $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+
+    public function getTourism ($index){
+        
+        $list = $this -> callApiList("TOURISM", $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+
+    public function getCulinary ($index){
+        
+        $list = $this -> callApiList("CULINARY", $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+
+    public function getSouvenir ($index){
+        
+        $list = $this -> callApiList("SOUVENIR", $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+    public function getLodging ($index){
+        
+        $list = $this -> callApiList("LODGING", $index);
+        if(sizeof($list)!= 0){
+            return $list;
+        }
+        return [];
+    }
+
+    function callApiDetail($mode, $page, $index){
+        $baseURL = $baseURL = "https://tegal-city.kaedenoki.net";
+        $client = new Client();
+
+        switch($mode){
+            case "NEWS" : 
+                $request = $client->get($baseURL."/berita/detail/".$page."/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["object"];
+                break;
+            case "EVENT" : 
+                $request = $client->get($baseURL . "/event/detail/".$page."/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["object"];
+                break;
+            case "TOURISM" : 
+                $request = $client->get($baseURL . "/pariwisata/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["data"];
+                break;
+            case "CULINARY" : 
+                $request = $client->get($baseURL . "/kuliner/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["data"];
+                break;
+            case "SOUVENIR" : 
+                $request = $client->get($baseURL . "/oleh/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["data"];
+                break;
+            case "LODGING" : 
+                $request = $client->get($baseURL . "/penginapan/".$index);
+                $response = $request->getBody()->getContents();
+                $data = json_decode($response,true);
+                $list = $data["data"];
+                break;
+            default :
+                break;
+        }
+        return $list;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,18 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// Route API List
 Route::get("/news/{index}", "DashboardController@getNewsPage");
 Route::get("/event/{index}", "DashboardController@getEventPage");
 Route::get("/tourism", "DashboardController@getTourism");
 Route::get("/culinary", "DashboardController@getCulinary");
 Route::get("/souvenir", "DashboardController@getSouvenir");
 Route::get("/lodging", "DashboardController@getLodging");
+
+// Route API Detail
+Route::get("/news/{page}/{index}", "DetailController@getNews");
+Route::get("/event/{page}/{index}", "DetailController@getEvent");
+Route::get("/tourism/{index}", "DetailController@getTourism");
+Route::get("/culinary/{index}", "DetailController@getCulinary");
+Route::get("/souvenir/{index}", "DetailController@getSouvenir");
+Route::get("/lodging/{index}", "DetailController@getLodging");
