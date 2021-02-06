e633feedf4aa41e48000f73c05d844c7cdba7b04
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-02-06 20:41:05 +0700

    fix portfoliobox

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,49 +5,49 @@
     <div id="portfolio">
         <div class="container-fluid p-0">
             <div class="row no-gutters">
-                <div class="col-lg-4 col-sm-6 portfolio-box">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('news')}}">
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/berita.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/berita.jpg') }}" alt="" />
                         <div class="portfolio-box-caption">
                             <div class="project-name">Berita</div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6 portfolio-box">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('tourism')}}" >
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/wisata.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/wisata.jpg') }}" alt="" />
                         <div class="portfolio-box-caption">
                             <div class="project-name">Pariwisata</div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('event')}}">
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/event2.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/event2.jpg') }}" alt="" />
                         <div class="portfolio-box-caption">
                             <div class="project-name">Event</div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('lodging')}}">
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/penginapan.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/penginapan.jpg') }}" alt="" />
                         <div class="portfolio-box-caption">
                             <div class="project-name">Penginapan</div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('culinary')}}">
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/kuliner.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/kuliner.jpg') }}" alt="" />
                         <div class="portfolio-box-caption">
                             <div class="project-name">Kuliner</div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6">
+                <div class="col-lg-4 col-sm-6 text-center">
                     <a class="portfolio-box" href="{{route('souvenir')}}">
-                        <img class="img-fluid" width="600" height="500" src="{{ asset('assets/img/portfolio/thumbnails/ole2.jpg') }}" alt="" />
+                        <img class="img-fluid" width="100%" height="100%" src="{{ asset('assets/img/portfolio/thumbnails/ole2.jpg') }}" alt="" />
                         <div class="portfolio-box-caption p-3">
                             <div class="project-name">Oleh Oleh</div>
                         </div>
